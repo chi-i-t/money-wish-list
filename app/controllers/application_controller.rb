45b9dff6c3bc2c44c7281d1834f64d059e32dc24
chi-i-t <59216455+chi-i-t@users.noreply.github.com>
2020-01-21 19:31:21 +0900

    fix user update function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :incomes, :fixedcosts, :savings])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :incomes, :fixedcosts, :savings])
     end
     
 
