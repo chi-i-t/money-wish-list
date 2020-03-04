7a414e8e629e70fa81ef432eb607eeeb903e7659
chi-i-t <59216455+chi-i-t@users.noreply.github.com>
2020-03-05 01:14:35 +0900

    add pie-chart

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+    
   devise_scope :user do
     root :to => "devise/sessions#new"
   end
   resources :wishes, only: [:index, :create, :update, :destroy]
   resources :records, only: [:index, :create, :update, :destroy]
+  resources :users, only: :show
+  
+
 end
 
