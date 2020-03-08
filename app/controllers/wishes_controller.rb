d36762c0254a178ff0a9521840081631674f0cac
chi-i-t <59216455+chi-i-t@users.noreply.github.com>
2020-03-08 13:20:12 +0900

    リファクタリング

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,5 +1,7 @@
 class WishesController < ApplicationController
+  before_action :sign_in_required, only: [:index]
   before_action :set_wish, only: [:edit, :update, :destroy]
+
   def index
     @wish = Wish.new
     @wishes = current_user.wishes.order("month ASC")
@@ -18,43 +20,19 @@ class WishesController < ApplicationController
     @message = Message.find(ids.sample)
   end
   
+
   def create
     Wish.create(wish_params)
-    
-    # @wish = Wish.create(wish_params)
-    # if @wish.save
-      # respond_to do |format|
-        # format.json
-        # redirect_to wishes_path
-      # end
-    # else
-      # flash.now[:alert] = 'メッセージを入力してください'
-      # render :index 
-    # end
-
     redirect_to wishes_path
   end
 
-  # def show
-  #   @wish = Wish.new
-  #   @wishes = current_user.wishes.order("month ASC")
-    
-  #   if params[:id].present?
-  #     @wish = Wish.find(params[:id])
-  #   else
-  #     @wish = Wish.new
-  #   end
-
-  #   @total_cost = current_user.wishes.sum(:cost)
-  #   @user_balance = current_user.incomes - (current_user.fixedcosts + current_user.savings)
-  #   @wish_balance = @user_balance - @total_cost
-  # end
 
   def update
     @wish.update(wish_params)
     redirect_to wishes_path
   end
   
+
   def destroy
     @wish.destroy
     redirect_to request.referer
--- app/views/wishes/create.json.jbuilder
+++
@@ -1,4 +0,0 @@
-# json.month  @wish.month
-# json.content  @wish.content
-# json.cost  @wish.cost
-# json.user_id  @wish.user.id
