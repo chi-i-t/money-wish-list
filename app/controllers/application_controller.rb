7b966964186775b433b11ed270b3bae796a87a6f
chi-i-t <59216455+chi-i-t@users.noreply.github.com>
2020-03-09 23:04:59 +0900

    add Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  
+  before_action :basic_auth, if: :production?
+
   def after_sign_in_path_for(resource)
     '/wishes'
   end
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
 
   protected
 
@@ -29,9 +29,19 @@ class ApplicationController < ActionController::Base
 
   private
 
+    def production?
+      Rails.env.production?
+    end
+
+    def basic_auth
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      end
+    end
+
     def sign_in_required
       redirect_to new_user_session_url unless user_signed_in?
     end
 
-    
+
 end
